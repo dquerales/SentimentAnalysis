f728f4bbb09d68fcef66b73b99ae4562297ecdea
Daniel Querales <45567065+dquerales@users.noreply.github.com>
2023-03-19 11:53:29 -0400

    update formating

--- app.py
+++ app.py
@@ -1,35 +1,27 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar  5 15:06:01 2023
-
-@author: Daniel
-"""
-
 import streamlit as st
 import joblib
-
 
 #load model
 model = joblib.load(open('Models/pipe_clf_model_checkpoint.joblib', 'rb'))
 model_clf = model['pipeline_clf']
 
-
+#app title
 st.title('Sentiment Analysis')
 
+#input
 text = st.text_input('Write your text review:')
 
+#predict
 if st.button('Predict Sentiment', use_container_width=True):
     prediction = model_clf.predict([text])
-
     if prediction == 1:
         output = 'Positive'
     else:
-        output = 'Negative'
-        
+        output = 'Negative' 
     st.success(f'The predicted sentiment is {output}')
 
 with st.sidebar:
     st.subheader('About')
-    st.markdown('The data was obtained from: https://www.kaggle.com/ashishpatel26/sentimental-analysis-nlp for educational purposes only.')
+    st.info('The data was obtained from: https://www.kaggle.com/ashishpatel26/sentimental-analysis-nlp for educational purposes only.')
     st.markdown('This webapp was made by Daniel Querales (d.querales@gmail.com) using **Streamlit**')
 st.sidebar.image('https://streamlit.io/images/brand/streamlit-mark-color.png', width=50)
