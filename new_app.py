5451a237b89e2307867937a9eb20f623b37b48b1
unknown <d.querales@gmail.com>
2023-06-18 17:53:19 -0400

    old ap

--- new_app.py
+++ new_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import joblib
+from transformers import pipeline
 
 #load model
-model = joblib.load(open('Models/pipe_clf_model_checkpoint.joblib', 'rb'))
-model_clf = model['pipeline_clf']
+sentiment_pipeline = pipeline("sentiment-analysis")
 
 #app title
 st.title('Sentiment Analysis')
@@ -13,12 +13,8 @@ text = st.text_input('Write your text review:')
 
 #predict
 if st.button('Predict Sentiment', use_container_width=True):
-    prediction = model_clf.predict([text])
-    if prediction == 1:
-        output = 'Positive'
-    else:
-        output = 'Negative' 
-    st.success(f'The predicted sentiment is {output}')
+   st.write(sentiment_pipeline(text))
+
 
 with st.sidebar:
     st.subheader('About')
